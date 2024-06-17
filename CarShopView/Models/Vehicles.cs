24504ed08271bcda94b6b536c8b47ae9bbb67966
ThreeDP <davy.paulino@aluno.ifsp.edu.br>
2024-06-17 18:37:08 -0300

    feat: Add Models from vehicles

---
+++ CarShopView/Models/Vehicles.cs
@@ -0,0 +1,33 @@
+
+
+namespace CarShop.Models;
+
+public class Vehicle {
+
+    [JsonPropertyName("renavan")]
+    public string?              Renavan { get; set; }
+    
+    [JsonPropertyName("license_plate")]
+    public string?              License { get; set; }
+
+    [JsonPropertyName("brand")]
+    public string?              Brand { get; set; }
+    
+    [JsonPropertyName("model")]
+    public string?              Model { get; set; }
+
+    [JsonPropertyName("model_year")]
+    public DateTime             ModelYear { get; set; }
+
+    [JsonPropertyName("vehicle_type")]
+    public string?              VehicleType { get; set; }
+
+    [JsonPropertyName("year_manufacture")]
+    public DateTime             YearManufacture { get; set; }
+
+    [JsonPropertyName("description")]
+    public string?              Description { get; set; }
+    
+    [JsonPropertyName("situation")]
+    public string?              Situation { get; set; }
+}
