2947ca795a37497bdaee89b97a83ab978b04bd4d
ThreeDP <davy.paulino@aluno.ifsp.edu.br>
2024-06-20 11:47:00 -0300

    feat: att message errors

--- backend/carshop/CarShop/Controllers/CustomersController.cs
+++ backend/carshop/CarShop/Controllers/CustomersController.cs
@@ -67,7 +67,6 @@ public class CustomersController : ControllerBase
     }
 
     [HttpPost]
-    [ServiceFilter(typeof(CarShopLoggingFilter))]
     public ActionResult Post([FromBody] CustomerDB c)
     {
         try {
--- frontend/CarShopView/Models/Clients.cs
+++ frontend/CarShopView/Models/Clients.cs
@@ -12,17 +12,21 @@ public class Customer
   public int                Id { get; set; }
 
   [JsonPropertyName("name")]
+  [Required(ErrorMessage = "informe seu nome.")]
   public string?            Name { get; set; }
   
   [JsonPropertyName("url_profile")]
   public string?            UrlImage { get; set; }
 
+  [Required(ErrorMessage = "informe o tipo do seu documento.")]
   [JsonPropertyName("document_type")]
   public string?            DocType { get; set; }
 
+  [Required(ErrorMessage = "informe o número do seu documento.")]
   [JsonPropertyName("document_number")]
   public string?            DocNumber { get; set; }
 
+  [Required(ErrorMessage = "informe seu número de telefone ou celular.")]
   [JsonPropertyName("cellphone")]
   public string?            CellPhone { get; set; }
 }
