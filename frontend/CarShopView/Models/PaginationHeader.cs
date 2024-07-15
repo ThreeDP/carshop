687fa5fef447ed9cd3d5aba0b2be9566fa8aee92
ThreeDP <davy.paulino@aluno.ifsp.edu.br>
2024-07-15 15:52:51 -0300

    feat: new front-end

---
+++ frontend/CarShopView/Models/PaginationHeader.cs
@@ -0,0 +1,11 @@
+namespace CarShopView.Models;
+
+public class PaginationHeader
+{
+    public int          TotalCount { get; set; }
+    public int          PageSize { get; set; }
+    public int          CurrentPage { get; set; }
+    public int          TotalPages { get; set; }
+    public bool         HasNext { get; set; }
+    public bool         HasPrevious { get; set; }
+}
